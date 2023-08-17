0dd625fc1eea1461c37132d7efb25a1c712bcf96
mariana <mariana.korb.mk@gmail.com>
2023-08-17 17:01:27 -0300

    Teste Git

---
+++ src/notas/Soma.java
@@ -0,0 +1,21 @@
+package notas;
+
+import java.util.Scanner;
+
+public class Soma {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int soma = 0;
+		
+		for(int i=0; i < 5; i++) {
+			System.out.println("NOta do " + (i+1) + "° aluno");
+			soma = sc.nextInt();
+		}
+		
+		System.out.println("Soma: " + soma);
+		sc.close();
+	}
+
+}
